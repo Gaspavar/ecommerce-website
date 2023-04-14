edbfae02b09c120afd199e22c992c6d7c12caeb6
Gaspare Tortora <g.tortora2@gmail.com>
2023-04-13 20:19:01 -0400

    Stored product variants from API call

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -606,7 +606,25 @@ const getProductData = async ()=>{
     const productInfo = {
         title: response.product.title,
         description: response.product.description,
-        image: response.product.featuredImage.url
+        image: response.product.featuredImage.url,
+        firstVariant: {
+            title: response.product.variants.edges[0].node.title,
+            image: response.product.variants.edges[0].node.image.url,
+            price: response.product.variants.edges[0].node.price.amount,
+            currency: response.product.variants.edges[0].node.price.currencyCode
+        },
+        secondVariant: {
+            title: response.product.variants.edges[1].node.title,
+            image: response.product.variants.edges[1].node.image.url,
+            price: response.product.variants.edges[1].node.price.amount,
+            currency: response.product.variants.edges[1].node.price.currencyCode
+        },
+        thirdVariant: {
+            title: response.product.variants.edges[2].node.title,
+            image: response.product.variants.edges[2].node.image.url,
+            price: response.product.variants.edges[2].node.price.amount,
+            currency: response.product.variants.edges[2].node.price.currencyCode
+        }
     };
     return productInfo;
 };
--- src/api.js
+++ src/api.js
@@ -33,10 +33,30 @@ const query = gql`
 `;
 const response = await request("https://mock.shop/api", query);
 
-const productInfo = {
+const productInfo = 
+{
     title: response.product.title,
     description: response.product.description,
-    image: response.product.featuredImage.url
+    image: response.product.featuredImage.url,
+    firstVariant: {
+      title: response.product.variants.edges[0].node.title,
+      image: response.product.variants.edges[0].node.image.url,
+      price: response.product.variants.edges[0].node.price.amount,
+      currency: response.product.variants.edges[0].node.price.currencyCode
+    },
+  
+    secondVariant: {
+      title: response.product.variants.edges[1].node.title,
+      image: response.product.variants.edges[1].node.image.url,
+      price: response.product.variants.edges[1].node.price.amount,
+      currency: response.product.variants.edges[1].node.price.currencyCode
+    },
+    thirdVariant: {
+      title: response.product.variants.edges[2].node.title,
+      image: response.product.variants.edges[2].node.image.url,
+      price: response.product.variants.edges[2].node.price.amount,
+      currency: response.product.variants.edges[2].node.price.currencyCode
+    }
 }
 return productInfo;
 }
