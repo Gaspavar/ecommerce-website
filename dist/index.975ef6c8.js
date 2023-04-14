76e059d899f6bf830cce85912d39ec9326673f2a
Gaspare Tortora <g.tortora2@gmail.com>
2023-04-13 21:57:02 -0400

    Added image changing functionality depending on variant selected

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -561,22 +561,27 @@ var _apiJs = require("./api.js");
 const title = document.querySelector(".title");
 const description = document.querySelector(".description");
 const imageURL = document.querySelector(".main-image");
-const firstVariantImage = document.querySelector(".first-variant-image");
-const secondVariantImage = document.querySelector(".second-variant-image");
-const thirdVariantImage = document.querySelector(".third-variant-image");
+// const firstVariantImage = document.querySelector(".first-variant-image")
+// const secondVariantImage = document.querySelector(".second-variant-image")
+// const thirdVariantImage = document.querySelector(".third-variant-image")
 const firstVariantTitle = document.querySelector(".first-variant-title");
 const secondVariantTitle = document.querySelector(".second-variant-title");
 const thirdVariantTitle = document.querySelector(".third-variant-title");
+const variantSelection = document.querySelector("#variant-selection");
 (0, _apiJs.getProductData)().then((product)=>{
     title.textContent = product.title;
     description.textContent = product.description;
     imageURL.attributes["src"].textContent = product.image;
-    firstVariantImage.attributes["src"].textContent = product.firstVariant.image;
-    secondVariantImage.attributes["src"].textContent = product.secondVariant.image;
-    thirdVariantImage.attributes["src"].textContent = product.thirdVariant.image;
     firstVariantTitle.textContent = product.firstVariant.title;
     secondVariantTitle.textContent = product.secondVariant.title;
     thirdVariantTitle.textContent = product.thirdVariant.title;
+    // Writing a function to change image displayed depending on the variant selected
+    const changeImageURL = (variant)=>{
+        if (variant.target.value === "green") imageURL.attributes["src"].textContent = product.firstVariant.image;
+        else if (variant.target.value === "olive") imageURL.attributes["src"].textContent = product.secondVariant.image;
+        else if (variant.target.value === "ocean") imageURL.attributes["src"].textContent = product.thirdVariant.image;
+    };
+    variantSelection.addEventListener("change", changeImageURL);
 });
 
 },{"./api.js":"8Zgej"}],"8Zgej":[function(require,module,exports) {
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const title = document.querySelector(".title");
 const description = document.querySelector(".description");
 const imageURL = document.querySelector(".main-image");
 
-const firstVariantImage = document.querySelector(".first-variant-image")
-const secondVariantImage = document.querySelector(".second-variant-image")
-const thirdVariantImage = document.querySelector(".third-variant-image")
+// const firstVariantImage = document.querySelector(".first-variant-image")
+// const secondVariantImage = document.querySelector(".second-variant-image")
+// const thirdVariantImage = document.querySelector(".third-variant-image")
 
 const firstVariantTitle = document.querySelector(".first-variant-title")
 const secondVariantTitle = document.querySelector(".second-variant-title")
 const thirdVariantTitle = document.querySelector(".third-variant-title")
+
+const variantSelection = document.querySelector("#variant-selection")
 
 
 getProductData().then((product) => {
@@ -18,13 +20,25 @@ getProductData().then((product) => {
     description.textContent = product.description;
     imageURL.attributes["src"].textContent = product.image;
 
-    firstVariantImage.attributes["src"].textContent = product.firstVariant.image;
-    secondVariantImage.attributes["src"].textContent = product.secondVariant.image;
-    thirdVariantImage.attributes["src"].textContent = product.thirdVariant.image;
-    
     firstVariantTitle.textContent = product.firstVariant.title;
     secondVariantTitle.textContent = product.secondVariant.title;
     thirdVariantTitle.textContent = product.thirdVariant.title;
+
+    // Writing a function to change image displayed depending on the variant selected
+
+    const changeImageURL = (variant) => {
+        if (variant.target.value === "green") {
+            imageURL.attributes["src"].textContent = product.firstVariant.image;
+        } else if (variant.target.value === "olive") {
+            imageURL.attributes["src"].textContent = product.secondVariant.image;
+        } else if (variant.target.value === "ocean") {
+            imageURL.attributes["src"].textContent = product.thirdVariant.image;
+        }
+    }
+
+    variantSelection.addEventListener("change", changeImageURL)
 });
+
+
 
 
